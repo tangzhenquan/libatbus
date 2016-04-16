2db5e5664635998028788ca8ca2d939d1ffda6a5
owent <owt5008137@live.com>
2016-04-16 18:56:43 +0800

    rename utils lib to atframe_utils

--- 3rd_party/c_cpp_utils/libc_cpp_utils.cmake
+++
@@ -1,7 +0,0 @@
-﻿
-# =========== 3rdparty c_cpp_utils ==================
-include_directories(${3RD_PARTY_C_CPP_UTILS_INC_DIR})
-
-message(STATUS "c&cpp utils inc=${3RD_PARTY_C_CPP_UTILS_INC_DIR}")
-message(STATUS "c&cpp utils src=${3RD_PARTY_C_CPP_UTILS_INC_DIR}")
-message(STATUS "c&cpp utils libname=${3RD_PARTY_C_CPP_UTILS_LINK_NAME}")
