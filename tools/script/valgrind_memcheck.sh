dc0c8d955c7fa3bbeed815d8a495bdacc7565fb3
owent <owt5008137@live.com>
2017-11-10 17:36:35 +0800

    记录valgrind配合gdb使用的脚本

--- tools/script/valgrind_memcheck.sh
+++ tools/script/valgrind_memcheck.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-valgrind --leak-check=full --tool=memcheck --show-leak-kinds=all --log-file=memcheck.log --malloc-fill=0x5E "$@";
+# valgrind --vgdb=full --vgdb-error=0 --leak-check=full --tool=memcheck --show-leak-kinds=all --log-file=memcheck.log --malloc-fill=0x5E "$@";
+# valgrind --vgdb=yes --vgdb-error=0 --leak-check=full --tool=memcheck --show-leak-kinds=all --log-file=memcheck.log --malloc-fill=0x5E "$@";
+# then, run: gdb [executable-file] --eval-command="target remote | vgdb"
+valgrind --vgdb=yes --leak-check=full --tool=memcheck --show-leak-kinds=all --log-file=memcheck.log "$@";
